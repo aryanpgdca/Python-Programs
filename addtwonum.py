45f70a2509d53d36683888016e8324df6a99e264
Aryan Porwal <118986780+aryanpgdca@users.noreply.github.com>
2023-03-05 12:09:44 +0000

    h

--- addtwonum.py
+++ addtwonum.py
@@ -1,4 +1,13 @@
-int a = 10
-int b = 20
-int c = a+b
+a = 10
+b = 20
+c = a+b
 print("Sum = ",c)
+
+
+#input by user
+x = input("Type a number: ")
+y = input("Type another number: ")
+
+sum = int(x) + int(y)
+
+print("The sum is: ", sum)
